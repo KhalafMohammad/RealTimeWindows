1b2edf795c0b2ae6ffa70ef34300beb555ef9b3f
Mohammad Khalaf <37309730+KhalafMohammad@users.noreply.github.com>
2024-09-09 16:32:40 +0200

    Added SetThreadPriority

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,14 +7,11 @@
 #include <stdio.h>
 #include <TlHelp32.h>
 
+
 void Get_Process_Handle(DWORD ProcessAccessInfo, DWORD processID);
 void Set_Process_Priority(HANDLE hprocess, DWORD priorityclass);
 void Set_Process_Affinity(HANDLE process_handle, DWORD_PTR dwProcessAffinityMask);
 
-
-DWORD dwError, dwPriClass, processid;
-DWORD_PTR dwProcessAffinityMask = 0x000000000000001; // 0xE (1110) for the last 3 cores reserved for real time
-HANDLE hProcess;
 
 //sets the priority of the process to the desiered class
 /*
@@ -29,31 +26,79 @@ HANDLE hProcess;
 */
 #define PriorityClass HIGH_PRIORITY_CLASS
 #define ProcessID 15756
+
+
+
+DWORD dwError, dwPriClass;
+DWORD_PTR dwProcessAffinityMask = 0x000000000000003; // 0xE (1110) for the last 3 cores reserved for real time
 DWORD_PTR processAffinityMask;
 DWORD_PTR systemAffinityMask;
-
+HANDLE hProcess, hThreadSnap = INVALID_HANDLE_VALUE, hThread;
+THREADENTRY32 te32;
 
 
 int main(void)
 {
 	std::cout << "hello there....\n" << std::endl;
-	//asking for process handle and information
-	//HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+
 	Get_Process_Handle(PROCESS_ALL_ACCESS, ProcessID); //returns the hProcess handle and the processid
 
 
-	Set_Process_Priority(hProcess, PriorityClass);
+	Set_Process_Priority(hProcess, PriorityClass); //sets the priority of the process to the desiered class
 
-	Set_Process_Affinity(hProcess, dwProcessAffinityMask);
+	Set_Process_Affinity(hProcess, dwProcessAffinityMask); // set affinity mask (on how many cores do you want the procss to run)
 
+	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, ProcessID); //get the thread snapshot
+	if (hThreadSnap == INVALID_HANDLE_VALUE) {
+		_tprintf(TEXT("CreateToolhelp32Snapshot (of threads) failed with %d\n"), GetLastError());
+		return 1;
+	}
+	else { _tprintf(TEXT("CreateToolhelp32Snapshot (of threads) is successful\n")); }
+
+	te32.dwSize = sizeof(THREADENTRY32);
+	if (!Thread32First(hThreadSnap, &te32)) {
+		_tprintf(TEXT("Thread32First failed with %d\n"), GetLastError());
+		CloseHandle(hThreadSnap);
+		return 1;
+	}
+	else {
+		_tprintf(TEXT("Thread32First is successful\n"));
+		do {
+			if (te32.th32OwnerProcessID == ProcessID) {
+				hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, te32.th32ThreadID); //open the thread and get the handle
+				_tprintf(TEXT("  1   Thread handle   = %p\n"), hThread);
+				SetThreadPriority(hThread, THREAD_PRIORITY_ABOVE_NORMAL);//sets the priority of the thread to the desiered class
+				//SetThreadAffinityMask(hThread, dwProcessAffinityMask); // set affinity mask (on how many cores do you want the procss to run)
+				_tprintf(TEXT("  2   THREAD ID      = %lu\n"), te32.th32ThreadID);
+				_tprintf(TEXT("  3   Base priority  = %d\n"), te32.tpBasePri);
+				_tprintf(TEXT("  4   Delta priority = %d\n"), te32.tpDeltaPri);
+				//GetProcessAffinityMask(hThread, &processAffinityMask, &systemAffinityMask);
+				//_tprintf(TEXT("     Thread affinity mask = %x\n"), processAffinityMask);
+				_tprintf(TEXT("  5   Thread priority = %d\n"), GetThreadPriority(hThread));
+				CloseHandle(hThread);
+				if (hThread != NULL) {
+					SetThreadPriority(hThread, THREAD_PRIORITY_ABOVE_NORMAL);
+				}
+				else {
+					_tprintf(TEXT("Failed to open thread with ID %lu\n"), te32.th32ThreadID);
+				}
+				printf("\n");
+			}
+		} while (Thread32Next(hThreadSnap, &te32));
+	}
+	// Close the handle to the thread snapshot
+	CloseHandle(hThreadSnap);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
 Cleanup:
 	// Clean up
 	CloseHandle(hProcess);
-	;
+	CloseHandle(hThreadSnap);
+	CloseHandle(hThread);
 	return 0;
 }
 
-//function to get the process id + error handling
+//Gets the process id + error handling
 void Get_Process_Handle(DWORD ProcessAccessInfo, DWORD processID) {
 	//asking for process handle and information
 	hProcess = OpenProcess(ProcessAccessInfo, FALSE, processID);
@@ -62,15 +107,15 @@ void Get_Process_Handle(DWORD ProcessAccessInfo, DWORD processID) {
 		return;
 	}
 	else {
-		processid = GetProcessId(hProcess);
-		_tprintf(TEXT("Current process ID is %lu\n"), processid);
+		processID = GetProcessId(hProcess);
+		_tprintf(TEXT("Current process ID is %lu\n"), processID);
 		_tprintf(TEXT("Process handle is %p\n"), hProcess);
 	}
 }
 
-
+//sets the priority of the process to the desiered class
 void Set_Process_Priority(HANDLE process_handle, DWORD priorityclass) {
-	//sets the priority of the process to the desiered class
+
 	SetPriorityClass(process_handle, priorityclass);
 	if (!SetPriorityClass(process_handle, priorityclass)) {
 		_tprintf(TEXT("Failed to set priority class (%d)\n"), GetLastError());
@@ -83,8 +128,9 @@ void Set_Process_Priority(HANDLE process_handle, DWORD priorityclass) {
 	}
 }
 
+// set affinity mask (on how many cores do you want the procss to run)
 void Set_Process_Affinity(HANDLE process_handle, DWORD_PTR dwProcessAffinityMask) {
-	// set affinity mask (on how many cores do you want the procss to run)
+
 	if (!SetProcessAffinityMask(process_handle, dwProcessAffinityMask)) {
 		dwError = GetLastError();
 		if (ERROR_INVALID_PARAMETER == dwError) { // ERROR_INVALID_PARAMETER for a not configured peocessor
--- ConsoleApplication1/newtestclass.h
+++ ConsoleApplication1/newtestclass.h
@@ -1,22 +1,27 @@
 #pragma once
 
-
+//#include <stdio.h>
+//#include <TlHelp32.h>
+//#include <iostream>
+//#include <windows.h>
 //#include <processthreadsapi.h>
 //#include <wdm.h>
 //#include <Ntddk.h>
 //#include <Ntifs.h>
 //#include <tchar.h>
-
+//
+//
+//int priority = THREAD_PRIORITY_ABOVE_NORMAL;
 //namespace ConsoleApplication1 {
 //    class newtestclass
 //    {
 //
 //        void testfunc() {
 //
-//            if (NtSetInformationThread(GetCurrentThread(), ThreadBasePriority, (int*)realtime_tvalue, ULONG(sizeof(int))) == STATUS_SUCCESS) {
+//            if (NtSetInformationThread(GetCurrentThread(), 14, &priority, ULONG(sizeof(int))) == STATUS_SUCCESS) {
 //                _tprintf(TEXT("Thread information is SET!!\n"));
 //            }
-//            else if (NtSetInformationThread(GetCurrentThread(), ThreadBasePriority, (int*)realtime_tvalue, ULONG(sizeof(int))) == STATUS_INFO_LENGTH_MISMATCH) {
+//            else if (NtSetInformationThread(GetCurrentThread(), 14, &priority, ULONG(sizeof(int))) == STATUS_INFO_LENGTH_MISMATCH) {
 //                _tprintf(TEXT("Thread information is mismatched!!\n"));
 //            }
 //        }
