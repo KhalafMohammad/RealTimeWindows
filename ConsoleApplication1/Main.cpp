301086eb4bcf3d5c7f7413534b158eea6e2eb404
Mohammad Khalaf <37309730+KhalafMohammad@users.noreply.github.com>
2024-09-10 16:48:14 +0200

    updated code

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <windows.h>
+#include <psapi.h>
 #include <processthreadsapi.h>
 #include <tchar.h>
 #include <winbase.h>
@@ -24,11 +25,13 @@ void Set_Process_Affinity(HANDLE process_handle, DWORD_PTR dwProcessAffinityMask
 * PROCESS_MODE_BACKGROUND_END 0x200000
 * REALTIME_PRIORITY_CLASS 0x100 Xx-*NUKE*-xX {{DO NOT USE IT WITHOUT RESERVING CPU CORES}}
 */
+
 #define PriorityClass HIGH_PRIORITY_CLASS //priority class of the process you want to change
-#define ProcessID 15756 //process id of the process you want to change the priority of
+//#define ProcessID 18096 //process id of the process you want to change the priority of
+int ProcessID;
+wchar_t ProcessName[MAX_PATH]; //process name of the process you want to change the priority of
 
-
-
+//variables
 DWORD dwError, dwPriClass;
 DWORD_PTR dwProcessAffinityMask = 0x000000000000003; // 0xE (1110) for the last 3 cores reserved for real time
 DWORD_PTR processAffinityMask;
@@ -39,30 +42,31 @@ THREADENTRY32 te32;
 
 int main(void)
 {
-	std::cout << "hello there....\n" << std::endl;
-
+	std::cout << "- Please Enter Process ID...." << std::endl;
+	std::cin >> ProcessID;
 	Get_Process_Handle(PROCESS_ALL_ACCESS, ProcessID); //returns the hProcess handle and the processid
-
-
+	GetProcessImageFileName(hProcess, ProcessName, MAX_PATH); //returns the process name
+	std::wcout << L"- Process Path is " << ProcessName << std::endl;  //print the process id and the process name
+	
 	Set_Process_Priority(hProcess, PriorityClass); //sets the priority of the process to the desiered class
 
 	Set_Process_Affinity(hProcess, dwProcessAffinityMask); // set affinity mask (on how many cores do you want the procss to run)
 
 	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, ProcessID); //get the thread snapshot
 	if (hThreadSnap == INVALID_HANDLE_VALUE) {
-		_tprintf(TEXT("CreateToolhelp32Snapshot (of threads) failed with %d\n"), GetLastError());
+		_tprintf(TEXT("= CreateToolhelp32Snapshot (of threads) failed with %d\n"), GetLastError());
 		return 1;
 	}
-	else { _tprintf(TEXT("CreateToolhelp32Snapshot (of threads) is successful\n")); }
+	else { _tprintf(TEXT("- CreateToolhelp32Snapshot (of threads) is successful\n")); }
 
 	te32.dwSize = sizeof(THREADENTRY32);
 	if (!Thread32First(hThreadSnap, &te32)) {
-		_tprintf(TEXT("Thread32First failed with %d\n"), GetLastError());
+		_tprintf(TEXT("= Thread32First failed with %d\n"), GetLastError());
 		CloseHandle(hThreadSnap);
 		return 1;
 	}
 	else {
-		_tprintf(TEXT("Thread32First is successful\n"));
+		_tprintf(TEXT("- Thread32First is successful\n"));
 		do {
 			if (te32.th32OwnerProcessID == ProcessID) {
 				hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, te32.th32ThreadID); //open the thread and get the handle
@@ -76,12 +80,12 @@ int main(void)
 				//_tprintf(TEXT("     Thread affinity mask = %x\n"), processAffinityMask);
 				_tprintf(TEXT("  5   Thread priority = %d\n"), GetThreadPriority(hThread));
 				CloseHandle(hThread);
-				if (hThread != NULL) {
+				/*if (hThread != NULL) {
 					SetThreadPriority(hThread, THREAD_PRIORITY_ABOVE_NORMAL);
 				}
 				else {
 					_tprintf(TEXT("Failed to open thread with ID %lu\n"), te32.th32ThreadID);
-				}
+				}*/
 				printf("\n");
 			}
 		} while (Thread32Next(hThreadSnap, &te32));
@@ -103,13 +107,13 @@ void Get_Process_Handle(DWORD ProcessAccessInfo, DWORD processID) {
 	//asking for process handle and information
 	hProcess = OpenProcess(ProcessAccessInfo, FALSE, processID);
 	if (hProcess == NULL) {
-		_tprintf(TEXT("OpenProcess failed (%d)\n"), GetLastError());
+		_tprintf(TEXT("= OpenProcess failed (%d)\n"), GetLastError());
 		return;
 	}
 	else {
 		processID = GetProcessId(hProcess);
-		_tprintf(TEXT("Current process ID is %lu\n"), processID);
-		_tprintf(TEXT("Process handle is %p\n"), hProcess);
+		_tprintf(TEXT("- Current process ID is %lu\n"), processID);
+		_tprintf(TEXT("- Process handle is %p\n"), hProcess);
 	}
 }
 
@@ -118,13 +122,13 @@ void Set_Process_Priority(HANDLE process_handle, DWORD priorityclass) {
 
 	SetPriorityClass(process_handle, priorityclass);
 	if (!SetPriorityClass(process_handle, priorityclass)) {
-		_tprintf(TEXT("Failed to set priority class (%d)\n"), GetLastError());
+		_tprintf(TEXT("= Failed to set priority class (%d)\n"), GetLastError());
 	}
 	else if (GetPriorityClass(process_handle) == priorityclass) {
-		_tprintf(TEXT("Priority class is set to 0x%x\n"), priorityclass);
+		_tprintf(TEXT("- Priority class is set to 0x%x\n"), priorityclass);
 	}
 	else {
-		_tprintf(TEXT("Priority class is not set to 0x%x\n"), priorityclass);
+		_tprintf(TEXT("- Priority class is not set to 0x%x\n"), priorityclass);
 	}
 }
 
@@ -135,19 +139,19 @@ void Set_Process_Affinity(HANDLE process_handle, DWORD_PTR dwProcessAffinityMask
 		dwError = GetLastError();
 		if (ERROR_INVALID_PARAMETER == dwError) { // ERROR_INVALID_PARAMETER for a not configured peocessor
 			//_tprintf(TEXT("mask is cheanged succesfully!"));
-			_tprintf(TEXT("ERROR_INVALID Processor configuerd"));
+			_tprintf(TEXT("= ERROR_INVALID Processor configuerd\n"));
 		}
 		else {
-			_tprintf(TEXT("mask did'nt change :("));
+			_tprintf(TEXT("= mask did'nt change :("));
 			goto Cleanup;
 		}
 	}
 	else {
 		if (!GetProcessAffinityMask(process_handle, &processAffinityMask, &systemAffinityMask))
 		{
-			_tprintf(TEXT("Failed to get process affinity mask (%d)\n"), GetLastError());
+			_tprintf(TEXT("= Failed to get process affinity mask (%d)\n"), GetLastError());
 		}
-		_tprintf(TEXT("Current process affinity Mask is %x\n"), processAffinityMask);
+		_tprintf(TEXT("- Current process affinity Mask is %x\n"), processAffinityMask);
 	}
 
 Cleanup:
